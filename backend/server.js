564fd3fa7805eeb87c5a9554b7e412f7f336c529
louisep1 <89385533+louisep1@users.noreply.github.com>
2023-01-21 17:35:38 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,10 @@ if (process.env.NODE_ENV === 'production') {
   //   )
   // )
 
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, './frontend/build')))
   app.get('*', function (_, res) {
     res.sendFile(
-      path.join(__dirname, '../frontend/build/index.html'),
+      path.join(__dirname, './frontend/build/index.html'),
       function (err) {
         res.status(500).send(err)
       }
