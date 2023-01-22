b5b34c8755a12d315dafe32925457a1c66b96798
louisep1 <89385533+louisep1@users.noreply.github.com>
2023-01-22 10:02:11 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,29 +19,12 @@ app.use('/flyers', express.static(path.join(folder, '/flyers')))
 
 // Serve frontend:
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static(path.join(__dirname, '../frontend/build')))
-  // app.get('*', (req, res) =>
-  //   res.sendFile(
-  //     path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-  //   )
-  // )
-
-  app.use(express.static(path.join(__dirname, './frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, './', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
     )
-  )
-
-  // app.use(express.static(path.join(__dirname, '../frontend/build')))
-  // app.get('*', function (_, res) {
-  //   res.sendFile(
-  //     path.join(__dirname, '../frontend/build/index.html'),
-  //     function (err) {
-  //       res.status(500).send(err)
-  //     }
-  //   )
-  // })
+  ) 
 } else {
   app.get('/', (req, res) => res.send('Please set to production'))
 }
