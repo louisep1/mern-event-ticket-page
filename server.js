115bedddcbb9963653497639f5496930b1380aad
louisep1 <89385533+louisep1@users.noreply.github.com>
2023-01-22 10:26:24 +0900

    move server.js from ./backend to root

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
-const connectDB = require('./config/db')
-const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./backend/config/db')
+const { errorHandler } = require('./backend/middleware/errorMiddleware')
 
 connectDB()
 const app = express()
 
 app.use(express.json())
 
-app.use('/api/event', require('./routes/eventRoutes'))
-app.use('/api/user', require('./routes/userRoutes'))
-app.use('/api/flyer', require('./routes/flyerRoutes'))
+app.use('/api/event', require('./backend/routes/eventRoutes'))
+app.use('/api/user', require('./backend/routes/userRoutes'))
+app.use('/api/flyer', require('./backend/routes/flyerRoutes'))
 
 const folder = path.resolve()
 app.use('/flyers', express.static(path.join(folder, '/flyers')))
